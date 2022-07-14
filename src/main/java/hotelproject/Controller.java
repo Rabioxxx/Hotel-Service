34bef62f91f89534e9fadab3cbc295ab94b46a3d
Rabioxxx <szg.jasinski@gmail.com>
2022-07-14 19:07:58 +0200

    Interaction menu

--- src/main/java/hotelproject/Controller.java
+++ src/main/java/hotelproject/Controller.java
@@ -7,6 +7,33 @@ public class Controller {
 
     public void startMenu() {
         System.out.println("Welcome in the HotelApp menu.");
-        System.out.println("What would you like to do?");
+
+        while (true) {
+            System.out.println("Enter a command:");
+
+            interactionMenu();
+
+            char input = readChar();
+
+            switch (input){
+                case 'a':
+                    break;
+                default:
+                    System.out.println("Unknown command, please try again.");
+                    break;
+            }
+        }
+    }
+
+    private void interactionMenu() {
+        System.out.println("a - Show list of all rooms.");
+        System.out.println("b - Show list of unoccupied rooms.");
+        System.out.println("c - Make a reservation.");
+        System.out.println("d - To check-out.");
+    }
+
+    private char readChar() {
+        Scanner s = new Scanner(System.in);
+        return s.next().charAt(0);
     }
 }
